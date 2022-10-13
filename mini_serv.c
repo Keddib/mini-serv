2f470869963bf9156befe572624631f79e70ec5a
keddib@student.1337.ma <keddib@student.1337.ma>
2022-10-13 13:23:55 +0100

    done

--- mini_serv.c
+++ mini_serv.c
@@ -16,6 +16,7 @@ typedef struct s_client
 {
   int id;
   int sock_fd;
+  char *buff;
 } t_client;
 
 typedef struct s_data
@@ -32,69 +33,61 @@ t_client *new_elem(int sockfd) {
   t_client *elm = malloc(sizeof(t_client) * 1);
   elm->sock_fd = sockfd;
   elm->id = client_id++;
+  elm->buff = NULL;
   return elm;
 }
 
 void delete_client(t_client **client) {
   close((*client)->sock_fd);
+  free((*client)->buff);
   free(*client);
   *client = NULL;
 }
 
-int ft_strlen(char *msg) {
-  int i = 0;
-  while (*msg) {
-    msg++;
-    i++;
-  }
-  return i;
-}
-
 int extract_message(char **buf, char **msg)
 {
-	char	*newbuf;
-	int	i;
-
-	*msg = 0;
-	if (*buf == 0)
-		return (0);
-	i = 0;
-	while ((*buf)[i])
-	{
-		if ((*buf)[i] == '\n')
-		{
-			newbuf = malloc(sizeof(*newbuf) * (ft_strlen(*buf + i + 1) + 1));
-			if (newbuf == 0)
-				return (-1);
-			strcpy(newbuf, *buf + i + 1);
-			*msg = *buf;
-			(*msg)[i + 1] = 0;
-			*buf = newbuf;
-			return (1);
-		}
-		i++;
-	}
-	return (0);
+  char    *newbuf;
+  int     i;
+  *msg = 0;
+  if (*buf == 0)
+          return (0);
+  i = 0;
+  while ((*buf)[i])
+  {
+          if ((*buf)[i] == '\n')
+          {
+                  newbuf = malloc(sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+                  if (newbuf == 0)
+                          return (-1);
+                  strcpy(newbuf, *buf + i + 1);
+                  *msg = *buf;
+                  (*msg)[i + 1] = 0;
+                  *buf = newbuf;
+                  return (1);
+          }
+          i++;
+  }
+  return (0);
 }
 
 char *str_join(char *buf, char *add)
 {
-	char	*newbuf;
-	int		len;
+        char    *newbuf;
+        int             len;
 
-	if (buf == 0)
-		len = 0;
-	else
-		len = ft_strlen(buf);
-	newbuf = malloc(sizeof(*newbuf) * (len + ft_strlen(add) + 1));
-	if (newbuf == 0)
-		return (0);
-	newbuf[0] = 0;
-	if (buf != 0)
-		strcat(newbuf, buf);
-	free(buf);
-	strcat(newbuf, add);
-	return (newbuf);
+        if (buf == 0)
+                len = 0;
+        else
+                len = strlen(buf);
+        newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+        if (newbuf == 0)
+                return (0);
+        newbuf[0] = 0;
+        if (buf != 0)
+                strcat(newbuf, buf);
+        free(buf);
+        strcat(newbuf, add);
+        return (newbuf);
 }
 
 int get_port(int argc, char **argv) {
@@ -108,38 +101,47 @@ int get_port(int argc, char **argv) {
 int init_socket(int port) {
 
   int sockfd;
-	struct sockaddr_in servaddr;
+        struct sockaddr_in servaddr;
 
-	// socket create and verification
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		write(2, "Fatal error\n", 13);
-		exit(1);
-	}
-	bzero(&servaddr, sizeof(servaddr));
+        // socket create and verification
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == -1) {
+                write(2, "Fatal error\n", 13);
+                exit(1);
+        }
+        bzero(&servaddr, sizeof(servaddr));
   // setsockopt(sockfd, SO_REUSEADDR, 1, 0, 0);
-	// assign IP, PORT
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	servaddr.sin_port = htons(port);
+        // assign IP, PORT
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+        servaddr.sin_port = htons(port);
 
-	// Binding newly created socket to given IP and verification
-	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
-		write(2, "Fatal error\n", 13);
-		exit(1);
-	}
+        // Binding newly created socket to given IP and verification
+        if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
+                write(2, "Fatal error\n", 13);
+                exit(1);
+        }
 
-	if (listen(sockfd, 10) != 0) {
-		write(2, "Fatal error\n", 13);
-		exit(1);
-	}
+        if (listen(sockfd, 10) != 0) {
+                write(2, "Fatal error\n", 13);
+                exit(1);
+        }
   return sockfd;
+}
+
+int ft_memcmp(const void *a, const void *b, size_t len) {
+  const char *p1 = (const char *)a;
+  const char *p2 = (const char *)b;
+  for (int i = 0; i < len; i++) {
+    if (p1[i] != p2[i])
+      return p1[i] - p2[i];
+  }
 }
 
 int FD_IS_ANY_SET(fd_set const *fdset)
 {
     static fd_set empty;     // initialized to 0 -> empty
-    return memcmp(fdset, &empty, sizeof(fd_set)) != 0;
+    return ft_memcmp(fdset, &empty, sizeof(fd_set)) != 0;
 }
 
 void broad_cast(t_data data, char *msg, int exept) {
@@ -151,16 +153,17 @@ void broad_cast(t_data data, char *msg, int exept) {
     return;
   }
   int set_len = select(data.maxfdp1, NULL, &clients, NULL, NULL);
-  printf("set_len %d\n", set_len);
+  // printf("set_len %d\n", set_len);
   for (int i = 0; i < data.maxc; i ++) {
     if (data.client_list[i] == NULL || data.client_list[i]->id == exept)
       continue;
     t_client *client = data.client_list[i];
     if (FD_ISSET(client->sock_fd, &clients)) {
-      printf("client %d is ready to Write\n",client->id);
-      int ret = send(client->sock_fd, msg, ft_strlen(msg), 0);
+      // printf("client %d is ready to Write\n",client->id);
+      int ret = send(client->sock_fd, msg, strlen(msg), 0);
       if (ret <= 0) {
-        printf("can\'t send\n");
+        // printf("can\'t send\n");
+        write(2, "Fatal error send\n", 13);
       }
       if (--set_len <= 0) // no more readable data
         break;
@@ -170,22 +173,24 @@ void broad_cast(t_data data, char *msg, int exept) {
 
 
 
-char *generate_message(char *buffer, int client_id) {
+char *generate_message(char *buffer, t_client *client) {
   char *msg = NULL;
   char *messages = NULL;
   int isMsg = 1;
   while (isMsg) {
     isMsg = extract_message(&buffer, &msg);
     if (isMsg == -1) {
-      printf("malloc failed\n");
+      write(2, "Fatal error\n", 13);
       exit(1);
     } else if (isMsg == 1) {
       char g_msg[BUFF_SIZE + 1];
-      sprintf(g_msg, "client %d: %s", client_id, msg);
+      sprintf(g_msg, "client %d: %s", client->id, str_join(client->buff, msg));
+      client->buff = NULL;
       free(msg);
       messages = str_join(messages, g_msg);
     }
   }
+  client->buff = str_join(client->buff, buffer);
   return messages;
 }
 
@@ -204,7 +209,7 @@ void init_data(t_data *data) {
 int main(int argc, char **argv) {
 
   t_data data;
-	data.sockfd  = init_socket(get_port(argc, argv));
+        data.sockfd  = init_socket(get_port(argc, argv));
   init_data(&data);
 
   for (;;) {
@@ -215,20 +220,19 @@ int main(int argc, char **argv) {
     // accept new connections
     if (FD_ISSET(data.sockfd, &read_set)) {
       // accept new client
-	    int connfd = accept(data.sockfd, 0, 0);
-	    if (connfd < 0) {
-        	write(2, "Fatal error\n", 13);
+            int connfd = accept(data.sockfd, 0, 0);
+            if (connfd < 0) {
+                write(2, "Fatal error\n", 13);
           exit(1);
       }
-      printf("fd : %d\n", connfd);
+      // printf("fd : %d\n", connfd);
       // add client to socket_set and clients collection with a new id
       t_client *new_client = new_elem(connfd);
       if (!new_client) {
-        // error
-        printf("malloc failed\n");
-        exit(-1);
+                    write(2, "Fatal error\n", 13);
+                    exit(1);
       }
-      printf("new client joinded : %d\n", new_client->id);
+      // printf("new client joinded : %d\n", new_client->id);
       char msg[BUFF_SIZE + 1];
       sprintf(msg, "server: client %d just arrived\n", new_client->id);
       broad_cast(data, msg, -1);
@@ -252,12 +256,12 @@ int main(int argc, char **argv) {
         continue;
       t_client *client = data.client_list[i];
       if (FD_ISSET(client->sock_fd, &read_set)) {
-        printf("client %d is ready to Read\n",client->id);
+        // printf("client %d is ready to Read\n",client->id);
         char *msg = NULL;
         char *buffer = malloc(sizeof(char) * BUFF_SIZE + 1);
         if (!buffer) {
-          printf("malloc failed\n");
-          exit(-1);
+                      write(2, "Fatal error\n", 13);
+                      exit(1);
         }
         int ret = recv(client->sock_fd, buffer, BUFF_SIZE + 1, 0);
         // ret == 0 -> the connection is closed
@@ -265,8 +269,8 @@ int main(int argc, char **argv) {
           // client left should be sent to all clients
           msg = malloc(sizeof(char) * BUFF_SIZE + 1);
           if (!msg) {
-            printf("malloc failed\n");
-            exit(1);
+            write(2, "Fatal error\n", 13);
+                        exit(1);
           }
           sprintf(msg, "server: client %d just left\n", client->id);
           broad_cast(data, msg, client->id);
@@ -276,9 +280,9 @@ int main(int argc, char **argv) {
           free(buffer);
         } else {
           buffer[ret] = 0x00;
-          buffer = generate_message(buffer, client->id);
+          buffer = generate_message(buffer, client);
           broad_cast(data, buffer, client->id);
-          free(buffer);
+          // free(buffer);
           // printf("client %d : %s\n", client->id, buffer);
         }
         if (--set_len <= 0) // no more readable data
